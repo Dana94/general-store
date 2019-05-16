75ed37d0e404868a80a2c6107858f9d0ff043990
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-05-16 17:36:22 -0400

    WIP

---
+++ src/components/Case.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <item></item>
+    <item></item>
+
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue';
+export default {
+  components: {
+    Item
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  div {
+    background-color: #d69657;
+    height: 200px;
+    width: 200px;
+    display: inline-block;
+  }
+</style>
+
